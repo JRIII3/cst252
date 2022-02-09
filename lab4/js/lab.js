83dfb2993413272446206cbb5b80263a99a5a684
JRIII3 <3iiijr3@gmail.com>
2022-02-09 00:01:43 -0800

    .

---
+++ lab4/js/lab.js
@@ -0,0 +1,13 @@
+//Find a safe place to park/stop
+  //Turn on hazard lights if parked on side of the road
+    //Grab materials/ tools needed
+      //Jack the car up
+        //Remove bolts off tire
+          //Remove tire
+            //Grab spare tire
+            //Add bolts on spare tire
+          //Jack the car down
+        //Tighten bolts and check tire pressure
+      //Put away materials/ tools
+    //Put away damaged tire
+    //Drive away
